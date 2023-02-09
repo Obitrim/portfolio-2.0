8cd4319ae44968f4018e4001ab0933d35c43d960
Obitrim <pevangelist516@gmail.com>
2023-02-09 03:34:13 +0000

    feat: create landing page structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,23 @@ import * as React from 'react';
 import Layout from '@/components/v1/layout/Layout';
 import Seo from '@/components/v1/Seo';
 
+import HomeBannerSection from '@/features/landing/banner/index.banner';
+import BlogsSection from '@/features/landing/blogs/index.blogs-section';
+import ProjectsSection from '@/features/landing/projects/index.projects-section';
+import TechnologiesSection from '@/features/landing/technologies/index.technologies';
+import TestimonialsSection from '@/features/landing/testimonials/index.testimonials';
+
 export default function HomePage() {
   return (
     <Layout>
-      {/* <Seo templateTitle='Home' /> */}
       <Seo title='Portfolio | Me' />
 
       <main>
-        <section></section>
+        <HomeBannerSection />
+        <ProjectsSection />
+        <TechnologiesSection />
+        <TestimonialsSection />
+        <BlogsSection />
       </main>
     </Layout>
   );
