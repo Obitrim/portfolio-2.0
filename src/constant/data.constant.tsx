e4cab909ba201851cf41fbea99956602b57bdbd6
Obitrim <pevangelist516@gmail.com>
2023-02-09 03:35:31 +0000

    feat: create projects and technologies used list

---
+++ src/constant/data.constant.tsx
@@ -0,0 +1,57 @@
+export const TECHNOLOGIES_AND_TOOLS = [
+  {
+    url: 'https://firebase.google.com/',
+    logo: 'https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg',
+  },
+  {
+    url: 'https://git-scm.com/',
+    logo: 'https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg',
+  },
+  {
+    url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+    logo: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg',
+  },
+  {
+    url: 'https://www.w3.org/html/',
+    logo: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg',
+  },
+  {
+    url: 'https://www.mongodb.com/',
+    logo: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg',
+  },
+  {
+    url: 'https://nextjs.org/',
+    logo: 'https://cdn.worldvectorlogo.com/logos/nextjs-2.svg',
+  },
+  {
+    url: 'https://nodejs.org',
+    logo: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg',
+  },
+  {
+    url: 'https://redux.js.org',
+    logo: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg',
+  },
+];
+
+export const PROJECTS = [
+  {
+    image: '/images/projects/flinchub.png',
+    url: 'https://dashboard.nzvc.co.nz/',
+  },
+  {
+    image: '/images/projects/lwm.png',
+    url: 'https://magicmynt.com/',
+  },
+  {
+    image: '/images/projects/flinchub.png',
+    url: 'https://dashboard.learnwithmochi.com/',
+  },
+  {
+    image: '/images/projects/lwm.png',
+    url: 'https://learnwithmochi.com/',
+  },
+  {
+    image: '/images/projects/flinchub.png',
+    url: 'https://flinchub.com/',
+  },
+];
